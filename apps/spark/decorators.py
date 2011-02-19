b78ca1261eebd10d8472b53e129942488c9aac3a
sork <franck.git@gmail.com>
2011-02-19 18:12:33 +0100

    Renamed playdoh.wsgi to spark.wsgi. Fixed ajax_required decorator's indentation

--- apps/spark/decorators.py
+++ apps/spark/decorators.py
@@ -103,25 +103,23 @@ def post_required(f):
     return wrapper
 
 
+def ajax_required(f):
+    """
+    AJAX request required decorator
+    use it in your views:
 
+    @ajax_required
+    def my_view(request):
+        ....
 
-    def ajax_required(f):
-        """
-        AJAX request required decorator
-        use it in your views:
-
-        @ajax_required
-        def my_view(request):
-            ....
-
-        """    
-        def wrap(request, *args, **kwargs):
-                if not request.is_ajax():
-                    return HttpResponseBadRequest()
-                return f(request, *args, **kwargs)
-        wrap.__doc__=f.__doc__
-        wrap.__name__=f.__name__
-        return wrap
+    """    
+    def wrap(request, *args, **kwargs):
+            if not request.is_ajax():
+                return HttpResponseBadRequest()
+            return f(request, *args, **kwargs)
+    wrap.__doc__=f.__doc__
+    wrap.__name__=f.__name__
+    return wrap
 
 
 def json_view(f):
