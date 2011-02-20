8617e2fccf3193c92c0c0c10efb11ec68155a11f
sork <franck.git@gmail.com>
2011-02-20 17:08:40 +0100

    Spark and badge sharing messages for l10n.

---
+++ apps/sharing/messages.py
@@ -0,0 +1,31 @@
+from tower import ugettext_lazy as _lazy
+
+
+# L10n: Spark sharing message via Twitter
+TWITTER_SPARK_MSG = _lazy(u'Hey! Help me spread Firefox for Mobile across the globe. '
+                           'Accept my Spark to help me earn points then join in the fun.')
+
+# L10n: Spark sharing message title via Facebook
+FACEBOOK_SPARK_TITLE = _lazy(u'Spark powered by Firefox for Mobile')
+
+# L10n: Spark sharing message content via Facebook
+FACEBOOK_SPARK_MSG = _lazy(u'Hi there, wanna help me light up the world? Help spread Firefox for '
+                            'mobile across the globe. Accept my Spark to help earn me points '
+                            'and join in the fun. Start your own Spark, take challenges, unlock '
+                            'badges and earn yourself some bragging rights.')
+
+# L10n: Spark sharing message via SMS
+SMS_SPARK_MSG = _lazy(u'Hi! Help me spread Firefox for Mobile across the globe. Accept my Spark '
+                       'and join in the fun. Start your own Spark, take challenges, unlock badges and more.')
+
+# L10n: Badge sharing message via Twitter
+TWITTER_BADGE_MSG = _lazy(u'I just unlocked the Spark %(badge_name)s badge. '
+                           'Check out all my badges %(short_url)s')
+
+# L10n: Badge sharing message title via Facebook
+FACEBOOK_BADGE_TITLE = _lazy(u'Spark powered by Firefox for Mobile')
+
+# L10n: Badge sharing message content via Facebook
+FACEBOOK_BADGE_MSG = _lazy(u'I just unlocked the Spark %(badge_name)s badge. Check out all my badges '
+                            'and join in the fun. Start your own Spark, take challenges, unlock badges '
+                            'and earn yourself some bragging rights.')
