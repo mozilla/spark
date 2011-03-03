b8b0aeefee031d64da13da6d1d2a5ea6c77b16c6
sork <franck.git@gmail.com>
2011-03-03 14:10:59 +0100

    Level description util function

--- apps/challenges/levels.py
+++ apps/challenges/levels.py
@@ -1,7 +1,19 @@
 from tower import ugettext_lazy as _lazy
 
+
 # L10n: Short description of any challenge level. Example: "Level 3"
 LEVEL_GENERIC = _lazy(u'Level %(num)d')
 
 # L10n: Name of the 'Super Sparker' secret level.
 LEVEL_5 = _lazy(u'The Super Sparker')
+
+
+
+def get_level_description(level):
+    desc = ''
+    if level == 5:
+        desc = unicode(LEVEL_5)
+    elif level >= 1 and level < 5:
+        desc = unicode(LEVEL_GENERIC) % dict(num=level)
+    
+    return desc
