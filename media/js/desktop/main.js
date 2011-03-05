388822837e72a723793650bdff95773a2aeec607
Guigui <heygui@gmail.com>
2011-03-05 15:16:23 +0100

    Removed unnecessary slider script

--- media/js/desktop/main.js
+++ media/js/desktop/main.js
@@ -155,24 +155,6 @@ $(document).ready(function() {
         }
     });
 
-    // Slider
-    var container = $('div#list-container');
-    var ul = $('#random-stats');
-
-    var itemsWidth = ul.innerWidth() - container.outerWidth();
-
-    $('#slider').slider({
-        min: 0,
-        max: itemsWidth,
-        handle: '#handle',
-        // stop: function (event, ui) {
-        //     ul.animate({'left' : ui.value * -1}, 500);
-        // },
-        slide: function (event, ui) {
-           ul.css('left', ui.value * -1);
-        }
-    });
-
     // Execute functions below
 
     initMask();
