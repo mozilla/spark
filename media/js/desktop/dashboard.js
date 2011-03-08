ea5dd72e03de0ca8e41927a43e0c836f9dc194e3
Guigui <heygui@gmail.com>
2011-03-08 22:54:50 +0100

    cleaned dashboard spark script

--- media/js/desktop/dashboard.js
+++ media/js/desktop/dashboard.js
@@ -45,85 +45,107 @@ var countUp = function(totalSeconds) {
     updateTime();
 };
 
-var canvas = document.getElementById('spark'),
-    ctx = canvas.getContext("2d"),
-    h = $('#spark-graphic').height(),
-	w = $('#spark-graphic').width(),
-	shapes = [];
+var initSpark = function (level) {
+    var canvas = document.getElementById('spark'),
+        ctx = canvas.getContext("2d"),
+        h = $('#spark-graphic').height(),
+    	w = $('#spark-graphic').width(),
+    	shapes = [],
+    	ignorelist = [];
 
-canvas.height = h;
-canvas.width = w;
-
-function deg2rad(degrees) {
-	return degrees * Math.PI/180;
-}
-
-function createShape(shape) {
-    shapes.push(shape);
-}
-
-function drawShapes(shapes, level) {
-    
-    var multiplier = 1 + (level / 10);
-    
-    if(multiplier < 1) {
-        //shapes.splice(6);
+    if(level === 1) {
+      ignorelist = [2, 4, 5, 8];
     }
-    
-    for(i = 0, nb = shapes.length; i < nb; i += 1) {
-        var shape = shapes[i];
 
-     	ctx.save();
-	    	ctx.fillStyle = "rgba("+shape.rgb+", 0.6)";
-		    ctx.translate(w / 2, h);
-	        ctx.rotate(deg2rad(shape.angle));
-	        ctx.beginPath();
-	        ctx.moveTo(0, 0);
-	        ctx.arc(0, 0, 100 * multiplier * shape.scale, 0, -deg2rad(shape.arcAngle), true);
-	        ctx.fill();
-	    ctx.restore();
+    if(level === 2) {
+      ignorelist = [2, 4, 8];
     }
-}
 
-function update() {
-    for(var i = 0, nb = shapes.length; i < nb; i += 1) {
-        var s = shapes[i];
-        
-        s.moveFactor = 1-(i/6);
-        
-        if(Math.abs(s.angle) > (s.maxAngle - s.arcAngle) || Math.abs(s.angle) < s.minAngle) {
-            s.angleStep *= -1;
-        } 
-        s.angle += s.angleStep;
-        
-        if(s.scale > s.maxScale || s.scale < s.minScale) {
-            s.scaleStep *= -1;
-        }
-        s.scale += s.scaleStep;
+    if(level === 3) {
+      ignorelist = [4, 8];  
     }
-}
 
-function initShapes(multiplier) {
-    createShape({ arcAngle: 100, angle: -80, angleStep: -0.1, minAngle: 80, maxAngle: 190, rgb: '255, 0, 60', scale: 0.85, scaleStep: 0.01, minScale: 0.8, maxScale: 0.95 });
-    createShape({ arcAngle: 35, angle: -120, angleStep: -0.3, minAngle: 70, maxAngle: 160, rgb: '255, 0, 60', scale: 0.95, scaleStep: 0.015, minScale: 0.9, maxScale: 1.1 });
-    createShape({ arcAngle: 155, angle: -10, angleStep: -0.5, minAngle: -30, maxAngle: 170, rgb: '255, 0, 60', scale: 0.8, scaleStep: 0.02, minScale: 0.65, maxScale: 0.9 });
-    createShape({ arcAngle: 45, angle: -77, angleStep: -2, minAngle: 50, maxAngle: 120, rgb: '255, 255, 0', scale: 0.6, scaleStep: 0.01, minScale: 0.5, maxScale: 0.7 });
-    createShape({ arcAngle: 95, angle: -15, angleStep: -1, minAngle: 10, maxAngle: 170, rgb: '255, 155, 0', scale: 0.75, scaleStep: 0.02, minScale: 0.65, maxScale: 0.85 });
-    createShape({ arcAngle: 45, angle: -45, angleStep: -1, minAngle: 40, maxAngle: 140, rgb: '255, 155, 0', scale: 0.6, scaleStep: 0.01, minScale: 0.55, maxScale: 0.75 });
+    if(level === 4) {
+      ignorelist = [4, 8];  
+    }
 
-    if (multiplier > 1.3) {
-        createShape({ arcAngle: 55, angle: -100, angleStep: -0.2, minAngle: 40, maxAngle: 140, rgb: '255, 155, 0', scale: 1, scaleStep: 0.02, minScale: 0.75, maxScale: 1 });
-        createShape({ arcAngle: 80, angle: -25, angleStep: -0.8, minAngle: 25, maxAngle: 155, rgb: '255, 155, 0', scale: 0.7, scaleStep: 0.015, minScale: 0.6, maxScale: 0.80 });
-        createShape({ arcAngle: 45, angle: -90, angleStep: -1, minAngle: 90, maxAngle: 170, rgb: '255, 255, 0', scale: 0.5, scaleStep: 0.01, minScale: 0.4, maxScale: 0.65 });
-        createShape({ arcAngle: 45, angle: -120, angleStep: -1, minAngle: 100, maxAngle: 180, rgb: '255, 255, 0', scale: 0.4, scaleStep: 0.015, minScale: 0.3, maxScale: 0.5 });
-        createShape({ arcAngle: 45, angle: -92, angleStep: -1.1, minAngle: 120, maxAngle: 180, rgb: '255, 255, 0', scale: 0.3, scaleStep: 0.025, minScale: 0.2, maxScale: 0.4 });
-        createShape({ arcAngle: 90, angle: -0, angleStep: -0.005, minAngle: 0, maxAngle: 95, rgb: '255, 255, 0', scale: 0.2, scaleStep: 0.01, minScale: 0.15, maxScale: 0.25 });
-        createShape({ arcAngle: 45, angle: -45, angleStep: -1, minAngle: 40, maxAngle: 140, rgb: '255, 155, 0', scale: 0.6, scaleStep: 0.01, minScale: 0.55, maxScale: 0.75 });
-    } else {
-        if(multiplier >= 1) {
-            createShape({ arcAngle: 45, angle: -90, angleStep: -1, minAngle: 90, maxAngle: 170, rgb: '255, 255, 0', scale: 0.5, scaleStep: 0.01, minScale: 0.4, maxScale: 0.65 });
-            createShape({ arcAngle: 45, angle: -120, angleStep: -1, minAngle: 100, maxAngle: 180, rgb: '255, 255, 0', scale: 0.4, scaleStep: 0.015, minScale: 0.3, maxScale: 0.5 });
-            createShape({ arcAngle: 90, angle: -0, angleStep: -0.005, minAngle: 0, maxAngle: 95, rgb: '255, 255, 0', scale: 0.2, scaleStep: 0.01, minScale: 0.15, maxScale: 0.25 });                
-        };
+    if(level === 5) {
+      ignorelist = [];
+    }
+
+    canvas.height = h;
+    canvas.width = w;
+
+    var deg2rad = function(degrees) {
+    	return degrees * Math.PI/180;
     };
+
+    var rand = function(min, max) {
+        return min + Math.floor(Math.random() * (max - min));
+    };
+
+    var drawShapes = function(shapes, level) {
+
+        var multiplier = 1 + (level / 10);
+
+        for(i = 0, nb = shapes.length; i < nb; i += 1) {
+            var shape = shapes[i];
+            
+            if(ignorelist.indexOf(i) === -1) {
+             	ctx.save();
+             	    ctx.shadowBlur = 30;
+             	    ctx.shadowColor = "rgba(0,0,0,0.3)";
+        	    	ctx.fillStyle = "rgba("+shape.rgb+", 0.6)";
+        		    ctx.translate(w / 2, h);
+        	        ctx.rotate(deg2rad(shape.angle));
+        	        ctx.beginPath();
+        	        ctx.moveTo(0, 0);
+        	        ctx.arc(0, 0, 100 * multiplier * shape.scale, 0, -deg2rad(shape.arcAngle), true);
+        	        ctx.fill();
+        	    ctx.restore();
+            }
+        }
+    };
+
+    var update = function() {
+        for(var i = 0, nb = shapes.length; i < nb; i += 1) {
+            var s = shapes[i];
+
+            s.moveFactor = 1-(i/6);
+
+            if(Math.abs(s.angle) > (s.maxAngle - s.arcAngle) || Math.abs(s.angle) < s.minAngle) {
+                s.angleStep *= -1;
+            } 
+            s.angle += s.angleStep * rand(1, 2);
+
+            if(s.scale > s.maxScale || s.scale < s.minScale) {
+                s.scaleStep *= -1;
+            }
+            s.scale += s.scaleStep * rand(1, 2);
+        }
+    };
+
+    var initShapes = function() {
+        shapes = [ { arcAngle: 100, angle: -90, angleStep: -0.1, minAngle: 80, maxAngle: 190, rgb: '255, 0, 60', scale: 0.85, scaleStep: 0.01, minScale: 0.8, maxScale: 0.95 },
+                    { arcAngle: 35, angle: -120, angleStep: -0.3, minAngle: 70, maxAngle: 160, rgb: '255, 0, 60', scale: 0.95, scaleStep: 0.015, minScale: 0.9, maxScale: 1.1 },
+                    { arcAngle: 155, angle: -10, angleStep: -0.5, minAngle: -30, maxAngle: 170, rgb: '255, 0, 60', scale: 0.8, scaleStep: 0.02, minScale: 0.65, maxScale: 0.9 },
+                    { arcAngle: 45, angle: -77, angleStep: -2, minAngle: 50, maxAngle: 120, rgb: '255, 255, 0', scale: 0.6, scaleStep: 0.01, minScale: 0.5, maxScale: 0.7 },
+                    { arcAngle: 95, angle: -15, angleStep: -1, minAngle: 10, maxAngle: 170, rgb: '255, 155, 0', scale: 0.75, scaleStep: 0.02, minScale: 0.65, maxScale: 0.85 },
+                    { arcAngle: 45, angle: -45, angleStep: -1, minAngle: 40, maxAngle: 140, rgb: '255, 155, 0', scale: 0.6, scaleStep: 0.01, minScale: 0.55, maxScale: 0.75 },
+                    { arcAngle: 45, angle: -90, angleStep: -1, minAngle: 90, maxAngle: 170, rgb: '255, 255, 0', scale: 0.5, scaleStep: 0.01, minScale: 0.4, maxScale: 0.65 },
+                    { arcAngle: 45, angle: -120, angleStep: -1, minAngle: 100, maxAngle: 180, rgb: '255, 255, 0', scale: 0.4, scaleStep: 0.015, minScale: 0.3, maxScale: 0.5 },
+                    { arcAngle: 90, angle: -0, angleStep: -0.005, minAngle: 0, maxAngle: 95, rgb: '255, 255, 0', scale: 0.2, scaleStep: 0.01, minScale: 0.15, maxScale: 0.25 } ];
+    };
+    
+    initShapes();
+
+    update();
+    
+    // drawShapes(shapes, level);
+    
+    setInterval(function() {
+        update();
+        ctx.clearRect(0, 0, w, h);
+        drawShapes(shapes, level);
+    }, 1000 / 30);
 }
