b773411f542ab05bf8bc72a453a791f72a651f72
Guigui <heygui@gmail.com>
2011-03-18 13:19:59 +0100

    Improvement of menu visibility

--- media/js/mobile/menu.js
+++ media/js/mobile/menu.js
@@ -22,3 +22,18 @@ var initMenu = function () {
         $('#menu-content').fadeIn('fast');
     });
 };
+
+var hintMenu = function() {
+    var $menu = $('#menu-wrapper'),
+        docHeight = $(document).height();
+
+    if(docHeight >= 500) {
+        $menu.css('top', -55);
+        setTimeout(function() {
+            $menu.animate({'top': '+=55'});
+        }, 1000);
+        $(window).scroll(function() {
+            $menu.css('top', 0);
+        });
+    }
+};
